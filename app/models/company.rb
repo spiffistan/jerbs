bb62a1450e6d8cf8086cb8fdd506d394450d2464
Christoffer Torris Olsen <chris@ctolsen.no>
2012-02-04 19:01:50 +0100

    Companies now have many jobs

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ActiveRecord::Base
   belongs_to :location
   has_many :divisions
+  has_many :jobs
 end
--- app/models/division.rb
+++ app/models/division.rb
@@ -1,2 +1,4 @@
 class Division < ActiveRecord::Base
+  belongs_to :company
+  has_many :jobs
 end
