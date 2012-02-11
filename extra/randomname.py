12cdcd3c186eaf2cae11f17358cd8497a7190100
Christoffer Torris Olsen <chris@ctolsen.no>
2012-02-11 19:22:49 +0100

    Revert "Revert f62230b256e56ae844d5cc5c0893dacd21c1ca71^..HEAD"
    This reverts commit c7e7ff887b71b194f79ded9e387101b1fe686580.

--- extra/randomname.py
+++ extra/randomname.py
@@ -1,54 +1,49 @@
 #!/usr/bin/env python
 
-import sys
-
 from random import random
 
-def randomname():
-	first = open('fornavn_by_freq.csv', 'r')
-	last = open('etternavn_by_freq.csv', 'r')
+first = open('fornavn_by_freq.csv', 'r')
+last = open('etternavn_by_freq.csv', 'r')
 
-	first_by_freq = []
-	last_by_freq = [] 
+first_by_freq = []
+last_by_freq = [] 
 
-	for line in first.readlines():
-	  name, freq = line.split()
-	  first_by_freq.append((float(freq),name))
+for line in first.readlines():
+  name, freq = line.split()
+  first_by_freq.append((float(freq),name))
  
-	for line in last.readlines():
-	  name, freq = line.split()
-	  last_by_freq.append((float(freq),name))
+for line in last.readlines():
+  name, freq = line.split()
+  last_by_freq.append((float(freq),name))
 
-	first_by_freq.sort()
-	last_by_freq.sort()
+first_by_freq.sort()
+last_by_freq.sort()
 
-	print first_by_freq
+print first_by_freq
 
-	def random_name():
-	  first_name = ""
-	  last_name = ""
+def random_name():
+  first_name = ""
+  last_name = ""
 
-	  acc = 0.0
-	  rand = random()
+  acc = 0.0
+  rand = random()
 
-	  for i in first_by_freq:
-	    acc += i[0]
-	    if rand < acc:
-	      first_name = i[1]
-	      break
+  for i in first_by_freq:
+    acc += i[0]
+    if rand < acc:
+      first_name = i[1]
+      break
 
-	  acc = 0.0
-	  rand = random()
+  acc = 0.0
+  rand = random()
 
-	  for i in last_by_freq:
-	    acc += i[0]
-	    if rand < acc:
-	      last_name = i[1]
-	      break
+  for i in last_by_freq:
+    acc += i[0]
+    if rand < acc:
+      last_name = i[1]
+      break
 
-	  return first_name + " " + last_name
+  return first_name + " " + last_name
 
-	print random_name()
+print random_name()
 
-if __name__ == "__main__":
-	sys.exit(randomname())
