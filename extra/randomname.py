f62230b256e56ae844d5cc5c0893dacd21c1ca71
Anders Aarsæther <anders@capasit.net>
2012-02-11 19:16:07 +0100

    Random name generator

---
+++ extra/randomname.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from random import random
+
+first = open('fornavn_by_freq.csv', 'r')
+last = open('etternavn_by_freq.csv', 'r')
+
+first_by_freq = []
+last_by_freq = [] 
+
+for line in first.readlines():
+  name, freq = line.split()
+  first_by_freq.append((float(freq),name))
+ 
+for line in last.readlines():
+  name, freq = line.split()
+  last_by_freq.append((float(freq),name))
+
+first_by_freq.sort()
+last_by_freq.sort()
+
+print first_by_freq
+
+def random_name():
+  first_name = ""
+  last_name = ""
+
+  acc = 0.0
+  rand = random()
+
+  for i in first_by_freq:
+    acc += i[0]
+    if rand < acc:
+      first_name = i[1]
+      break
+
+  acc = 0.0
+  rand = random()
+
+  for i in last_by_freq:
+    acc += i[0]
+    if rand < acc:
+      last_name = i[1]
+      break
+
+  return first_name + " " + last_name
+
+print random_name()
+
