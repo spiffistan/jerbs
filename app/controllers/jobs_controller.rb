df2449e621083e04c2704ee3d49c31f86d15ed3e
Christoffer Torris Olsen <chris@ctolsen.no>
2012-02-16 22:47:01 +0100

    Create job form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,22 @@ class JobsController < ApplicationController
   def list_results
     @jobs = Job.find(:all)
   end
+  
+  def new
+    @job = Job.new
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => @job }
+    end
+  end
+  
+  def create
+    @job = Job.new(params[:post])
+      if @job.save
+        format.html { redirect_to(@job)}
+    else
+      format.html { render :action => "new" }
+    end
+  end
 end
