da7831943be9678fcb845c6a661c8f3a1fa7a86e
Christoffer Torris Olsen <chris@ctolsen.no>
2012-02-17 23:20:49 +0100

    Updated annotations

--- test/fixtures/user_profiles.yml
+++ test/fixtures/user_profiles.yml
@@ -3,7 +3,6 @@
 # Table name: user_profiles
 #
 #  id          :integer         not null, primary key
-#  user_id     :integer
 #  name        :string(255)
 #  url         :string(255)
 #  location_id :integer
--- test/unit/technology_test.rb
+++ test/unit/technology_test.rb
@@ -2,11 +2,11 @@
 #
 # Table name: technologies
 #
-#  id          :integer         not null, primary key
-#  name        :string(255)
-#  description :string(255)
-#  created_at  :datetime        not null
-#  updated_at  :datetime        not null
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#  shortname  :string(255)
 #
 
 require 'test_helper'
