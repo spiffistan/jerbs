82b114ec9b8f5122245f858216c27588f5720eb7
Anders Aarsæther <anders@capasit.net>
2012-03-25 22:01:29 +0200

    Expanded sortable helper

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,7 +1,17 @@
 module JobsHelper
-  def link_to_sortable(column, title = nil, cssclasses = nil)
+  def link_to_sortable(column, title = nil, css_classes = nil)
     title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction} #{css_classes}" : nil
     direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-    link_to title, { :sort => column, :direction => direction }, :class => cssclasses
+
+    html_link = link_to title, { :sort => column, :direction => direction }, :class => css_class
+
+    if column == sort_column and direction == 'asc'
+      html_link = html_link + ' <i class="icon-chevron-up"></i>'.html_safe
+    elsif column == sort_column and direction == 'desc'
+      html_link = html_link + ' <i class="icon-chevron-down"></i>'.html_safe
+    end
+
+    return html_link
   end
 end
