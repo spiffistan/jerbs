eac7b8ba9bdada031c68c26f863addc4d055d712
Anders Aarsæther <anders@capasit.net>
2012-03-26 16:23:05 +0200

    Echoing in boot script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo
+echo '--- Installing bundle:'
 bundle install
+echo
+echo '--- Migrating:'
 rake db:migrate
+echo
+echo '--- Starting solr:'
 rake sunspot:solr:start
+echo
+echo '--- Populating database:'
 rake db:populate
